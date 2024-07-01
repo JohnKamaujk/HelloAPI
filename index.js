4c77195a51869483e9b8de2d482e973b12245068
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-01 23:25:58 +0300

    :sparkles:get user location and weather

--- index.js
+++ index.js
@@ -8,30 +8,38 @@ const port = 3000;
 app.set("trust proxy", true);
 app.use(cors());
 
-//welcome route
+// Welcome route
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
 // /api/hello?visitor_name=""
 app.get("/api/hello", async (req, res) => {
-  const visitorName = req.query.visitor_name;
-  const ipAddress = req.ip;
-  const weatherApiKey = "986ff9178702426d863182145240107";
+  try {
+    const visitorName = req.query.visitor_name;
+    const ipAddress = req.ip;
+    const weatherApiKey = "986ff9178702426d863182145240107";
 
-  // Fetch IP location and weather information using WeatherAPI
-  const locationUrl = `http://api.weatherapi.com/v1/ip.json?key=${weatherApiKey}&q=${ipAddress}`;
-  const locationResponse = await axios.get(locationUrl);
-  const { lat, lon } = locationResponse.data;
+    // Fetch IP location using WeatherAPI
+    const locationUrl = `http://api.weatherapi.com/v1/ip.json?key=${weatherApiKey}&q=${ipAddress}`;
+    const locationResponse = await axios.get(locationUrl);
+    const { lat, lon, city } = locationResponse.data;
 
-  const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lon}`;
-  const weatherResponse = await axios.get(weatherUrl);
+    // Fetch weather information using WeatherAPI
+    const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lon}`;
+    const weatherResponse = await axios.get(weatherUrl);
+    const temperature = weatherResponse.data.current.temp_c;
 
-  res.json({
-    client_ip: ipAddress,
-    location: locationResponse.data.city,
-    greeting: `Hello, ${visitorName}!, the temperature is ${weatherResponse.data.current.temp_c} degrees Celsius in ${locationResponse.data.city}`,
-  });
+    res.json({
+      client_ip: ipAddress,
+      location: city,
+      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}`,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch location or weather information" });
+  }
 });
 
 // Start the server
