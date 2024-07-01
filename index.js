757e15154cf6b8e3ac310a5a54a529be941f6e6f
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-01 22:08:11 +0300

    :sparkles:get user ip

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// Enable CORS
+app.set("trust proxy", true);
 app.use(cors());
 
 // Define a route
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+  const ipAddress = req.ip;
+  return res.json({ message: `Hello! Your IP address is: ${ipAddress}` });
 });
 
 // Start the server
