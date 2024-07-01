59e7b2c756c650e7589cc62b3f65b550d10c0c18
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-01 22:57:46 +0300

    :sparkles:get user location and weather

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ app.set("trust proxy", true);
 app.use(cors());
 
 // Define a route
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   const ipAddress = req.ip;
-  return res.json({ message: `Hello! Your IP address is: ${ipAddress}` });
+  const weatherApiKey = "986ff9178702426d863182145240107";
+
+  // Fetch IP location and weather information using WeatherAPI
+  const locationUrl = `http://api.weatherapi.com/v1/ip.json?key=${weatherApiKey}&q=${ipAddress}`;
+  const locationResponse = await axios.get(locationUrl);
+  const { lat, lon } = locationResponse.data;
+
+  const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${lat},${lon}`;
+  const weatherResponse = await axios.get(weatherUrl);
+
+  res.json({
+    message: "Hello World",
+    ip: ipAddress,
+    location: locationResponse.data,
+    weather: weatherResponse.data,
+  });
 });
 
 // Start the server
